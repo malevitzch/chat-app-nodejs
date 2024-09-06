deba8763e0c3d94458545e2daf31a79f7454a33c
malevitzch <malevitzch@gmail.com>
2024-09-06 20:04:02 +0200

    Work on integrating the database

--- app/backend/flask/app.py
+++ app/backend/flask/app.py
@@ -1,7 +1,40 @@
 from flask import Flask, jsonify
-
+import os
+import sqlite3
 app = Flask(__name__)
 
+DBPATH = "./database.db"
+
+def init_db():
+    with sqlite3.connect(DBPATH) as connection:
+        sql_statements = [
+            """CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                contents TEXT NOT NULL
+            );""",
+            """CREATE TRIGGER limit_messages AFTER INSERT ON messages
+                BEGIN
+                    DELETE FROM messages WHERE id NOT IN (SELECT id FROM messages ORDER BY id DESC LIMIT X);
+                END;"""]
+        cursor = connection.cursor()
+        for statement in sql_statements:
+            cursor.execute(statement)
+        connection.commit()
+        connection.close()
+def db_get() -> str:
+    if not os.path.exists(DBPATH):
+        init_db()
+    with sqlite3.connect(DBPATH) as connection:
+        return ""
+
+def db_post(message: str):
+    if not os.path.exists(DBPATH):
+        init_db()
+    with sqlite3.connect(DBPATH) as connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO messages (contents) VALUES (?);", (message,))
+        connection.commit()
+        connection.close()
 MAX_MESSAGES = 3
 messages = []
 
@@ -15,7 +48,7 @@ def post_message(message):
     global messages
     messages.append(message)
     clear_old()
-    return jsonify(messages)
+    return db_get()
 
 @app.route("/get")
 def get_messages():
