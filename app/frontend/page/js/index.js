4a24820c85e0851d4a956482ff1c2416e058a313
malevitzch <malevitzch@gmail.com>
2024-09-07 14:18:40 +0200

    Renamed main.js to index.js

---
+++ app/frontend/page/js/index.js
@@ -0,0 +1,33 @@
+const MAX_MESSAGES = 3
+
+list = document.getElementById('msgList');
+
+for(let i = 0; i < MAX_MESSAGES; i++)
+{
+    var li = document.createElement('li');
+    list.appendChild(li);
+}
+
+async function setup() {
+    let req;
+    await fetch('/api/get')
+	.then(response => response.json())
+	.then(data => req = data);
+    const items = list.getElementsByTagName('li');
+    for (let i = 0; i < req.length; i++) 
+    {
+	items[i].textContent = req[i];
+    }
+}
+
+async function post(event)
+{
+    const inputString = document.getElementById('messageform').value;
+
+    await fetch('/api/post/' + inputString)
+	.then(response => response.json());
+    console.log('Submitted value:', inputValue);
+    setup();
+}
+
+setInterval(async () => {setup()}, 500);
