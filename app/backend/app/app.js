b3d36d2930dff7315dcd7bbf03dff895b7728184
malevitzch <malevitzch@gmail.com>
2024-09-16 09:57:02 +0200

    Slight refactoring, added full documentation to the main app.js file

--- app/backend/app/app.js
+++ app/backend/app/app.js
@@ -1,24 +1,36 @@
+//importing socketio
 const { Server } = require('socket.io');
 
+//importing sqlite3, verbose means extra diagnostics
 const sqlite3 = require('sqlite3').verbose();
-const dbm = require('./database.js');
- 
-const db = dbm.init();
+
+//import the database api file
+const db = require('./database.js');
+
+//creating the database
+const database = db.init();
+
+//initializing a socketio server to handle websocket connections
 const io = new Server(8000, {
   cors: {
     origin: "*",
   }
 });
 
-console.log('Socket.IO server is running on port 8000');
 io.on('connection', (socket) => {
+  //on every connection we output that there is a successful connection (this is mostly for debugging purposes)
   console.log("SUCCESSFUL CONNECTION");
+  
+  //whenever a post_msg message is emitted, we put it into the database
   socket.on('post_msg', (msg) => {
-    dbm.post(db, msg);
+    db.post(database, msg);
+    //inform all the currently connected users that a new message is available
     io.emit('new');
   });
   socket.on('get_msgs', async () => {
-    const response = await dbm.get(db);
+    //wait for the async db.get function
+    const response = await db.get(database);
+    //send the update back to the user who requested it
     socket.emit('update', response);
   });
 });
