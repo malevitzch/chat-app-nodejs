b3a70a52169647e21acc63fa258250145164e77f
malevitzch <malevitzch@gmail.com>
2024-09-17 10:44:55 +0200

    Initial setup is now properly done on window load, which means that the scripts can finally be moved to head rather than being at the end of body

--- app/frontend/page/js/index.js
+++ app/frontend/page/js/index.js
@@ -4,6 +4,8 @@ const MAX_MESSAGES = 10
 let msg_div;
 let list;
 
+//TODO: use require with colorscheme
+
 window.addEventListener('load', function() {
     msg_div = document.getElementById('msgBox');
     list = msg_div.querySelector('#msgList');
@@ -11,51 +13,24 @@ window.addEventListener('load', function() {
 	var li = document.createElement('li');
 	list.appendChild(li);
     }
+    //preventing default form behavior, basically forms used to reload the page and reset colorscheme on submit
+    document.querySelector('form').addEventListener('submit', function(event) {
+	event.preventDefault();
+    });
 });
 
-//preventing default form behavior, basically forms used to reload the page and reset colorscheme on submit
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-});
-
-//the colorscheme switch
-document.getElementById("cont").addEventListener("click", function() {
-    document.getElementById("tog").classList.toggle("active");
-    if(document.getElementById("tog").classList.contains("active")) {
-	set_color_scheme(colorscheme_orange);
-    } else {
-	set_color_scheme(colorscheme_default);
-    }
-});
-
-//give focus to the text field on page load
 window.addEventListener('load', function() {
-    document.getElementById("messageform").focus();
+    //the colorscheme switch
+    document.getElementById("cont").addEventListener("click", function() {
+	document.getElementById("tog").classList.toggle("active");
+	if(document.getElementById("tog").classList.contains("active")) {
+	    set_color_scheme(colorscheme_orange);
+	} else {
+	    set_color_scheme(colorscheme_default);
+	}
+    });
 });
 
-//initializing the socket
-const hostname = document.location.hostname;
-const address =`http://${hostname}:8000`;
-const socket = io(address);
-
-//filling the list with recieved data upon recieving an 'update' message through the socket
-socket.on('update', (data) => {
-    //access the list of items from the list
-    //TODO: error handling in case the database somehow returns more than MAX_MESSAGES 
-    const items = list.getElementsByTagName('li');
-    for (let i = 0; i < data.length; i++) 
-    {
-	items[i].textContent = data[i].contents;
-    }
-});
-
-//whenever the server has new data, it sends a 'new' message to the client
-socket.on('new', () => {
-    //the client emits the get_msgs message upon recieving the information that there is new data
-    socket.emit('get_msgs');
-});
-
-//the function used by the form to post messages
 function post() {
     //get the message value from the form
     const msg = document.getElementById('messageform').value;
@@ -65,5 +40,42 @@ function post() {
     socket.emit('post_msg', msg);
 }
 
-//initial emisson of get_msgs so that we get them on page load
-socket.emit('get_msgs');
+//give focus to the text field on page load
+window.addEventListener('load', function() {
+    document.getElementById("messageform").focus();
+});
+
+let hostname;
+let address;
+let socket;
+
+//handle websocket stuff
+window.addEventListener('load', function() {
+    //initializing the socket
+    hostname = document.location.hostname;
+    address =`http://${hostname}:8000`;
+    socket = io(address);
+
+    //filling the list with recieved data upon recieving an 'update' message through the socket
+    socket.on('update', (data) => {
+	//access the list of items from the list
+	//TODO: error handling in case the database somehow returns more than MAX_MESSAGES 
+	const items = list.getElementsByTagName('li');
+	for (let i = 0; i < data.length; i++) 
+	{
+	    items[i].textContent = data[i].contents;
+	}
+    });
+
+    //whenever the server has new data, it sends a 'new' message to the client
+    socket.on('new', () => {
+	//the client emits the get_msgs message upon recieving the information that there is new data
+	socket.emit('get_msgs');
+    });
+
+    //the function used by the form to post messages
+
+    //initial emisson of get_msgs so that we get them on page load
+    socket.emit('get_msgs');
+});
+
