b5246cdb0710a478bbf3bb21aa7a720403b790f1
malevitzch <malevitzch@gmail.com>
2025-10-13 13:57:18 +0200

    Revamped the way the database is used, no longer relying on the limited trigger functionality

--- app/backend/app/database.js
+++ app/backend/app/database.js
@@ -1,9 +1,16 @@
-// Handles how much hisotry is kept
-const MESSAGE_LIMIT = process.env.MESSAGE_LIMIT || 10;
+// The maximum number of messages contained in a response to an user
+const MESSAGE_LIMIT = parseInt(process.env.MESSAGE_LIMIT || 10);
+
+// Handles how much hisotry is kept server-side
+const HISTORY_SIZE = Math.max(MESSAGE_LIMIT, parseInt(process.env.HISTORY_LIMIT || 10));
+
 let last_index = 0;
 // Handles how many extra messages can be stored into the database before we
 // perform a clean. Allows to trade disk space for speed
-const EXTRA_BEFORE_DELETION = process.env.EXTRA_BEFORE_DELETION || 0;
+const EXTRA_BEFORE_DELETION = parseInt(process.env.EXTRA_BEFORE_DELETION || HISTORY_SIZE);
+
+
+const DELETE_THRESHOLD = HISTORY_SIZE + EXTRA_BEFORE_DELETION;
 
 // importing sqlite3 library, verbose is for better diagnostics
 const sqlite3 = require('sqlite3').verbose();
@@ -13,32 +20,26 @@ function init() {
   // creating a new database in-memory
   const database = new sqlite3.Database(':memory:');
 
-  // running the necessary initialization queries, serialize makes them run synchronously
-  database.serialize(() => {
-    // creating the table
-    database.run(`CREATE TABLE IF NOT EXISTS messages 
-          (id INTEGER PRIMARY KEY, contents TEXT NOT NULL, sender TEXT);`
-          );
-    // adding a trigger that removes all messages beyond a certain limit on insert
-    let LIMIT = parseInt(MESSAGE_LIMIT, 10) + parseInt(EXTRA_BEFORE_DELETION, 10);
-    database.run(`CREATE TRIGGER limit_messages AFTER INSERT ON messages
-          WHEN (SELECT COUNT(*) FROM messages) > ${MESSAGE_LIMIT}
-          BEGIN
-            DELETE FROM messages WHERE id NOT IN
-            (SELECT id FROM messages ORDER BY id DESC LIMIT ${MESSAGE_LIMIT});
-          END;`
-            );
-  });
+  // creating the table
+  database.run(`CREATE TABLE IF NOT EXISTS messages 
+        (id INTEGER PRIMARY KEY, contents TEXT NOT NULL, sender TEXT);`);
   return database;
+}
+
+function cleanup(database) {
+  database.run(`
+    DELETE FROM messages
+    WHERE id <= (?)
+  `, [last_index - HISTORY_SIZE]);
 }
 
 // auxiliary function for getting messages from db
 function fetch(database) {
   return new Promise((resolve, reject) => {
-    database.all(`SELECT contents FROM messages ORDER BY ID ASC LIMIT ?;`,
-                 [MESSAGE_LIMIT], (err, rows) => {
-      resolve(rows);
-    });
+    database.all(`SELECT contents FROM messages WHERE id > (?) ORDER BY id ASC LIMIT (?);`,
+      [last_index - MESSAGE_LIMIT, MESSAGE_LIMIT], (err, rows) => {
+        resolve(rows);
+      });
   });
 }
 
@@ -49,16 +50,17 @@ async function get(database) {
 }
 
 // API function that inserts a new message into the database
-function post(database, msg, sender=null) {
+function post(database, msg, sender = null) {
   last_index += 1;
-  if(sender==null) {
-    database.run(`INSERT INTO messages (ID, contents) VALUES (?, ?);`, 
+  if (sender == null) {
+    database.run(`INSERT INTO messages (ID, contents) VALUES (?, ?);`,
       [last_index, msg]);
   } else {
     database.run(`INSERT INTO messages (ID, contents, sender) VALUES (?, ?, ?);`,
       [last_index, msg, sender]);
   }
 }
+
 
 // exported functions, notice we don't export fetch because it is auxiliary 
 module.exports = {
