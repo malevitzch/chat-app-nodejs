b2d661c18af24af3a8105588a657bf3216aa5c97
malevitzch <malevitzch@gmail.com>
2025-10-21 21:24:03 +0200

    Updated dependencies, started writing some code

--- app/backend-rust/app/src/main.rs
+++ app/backend-rust/app/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .enable_all()
+        .build()
+        .unwrap();
+    /*
+        rt.block_on(async {
+            run_server().await;
+        });
+    */
 }
