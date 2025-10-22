3c42072e17be4c69c48a4d948aedc20f6a02c8bc
malevitzch <malevitzch@gmail.com>
2025-10-22 20:24:34 +0200

    The rust backet backend now runs a simple http server and actually serves something

--- app/backend-rust/app/src/main.rs
+++ app/backend-rust/app/src/main.rs
@@ -1,3 +1,22 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+async fn hello() -> &'static str {
+    "Welcome"
+}
+
+fn create_router() -> Router {
+    Router::new().route("/", get(hello))
+}
+
+async fn run_server() {
+    let app = create_router();
+    let address = SocketAddr::from(([0, 0, 0, 0], 8000));
+    axum::serve(tokio::net::TcpListener::bind(address).await.unwrap(), app)
+        .await
+        .unwrap();
+}
+
 fn main() {
     println!("Hello, world!");
     let rt = tokio::runtime::Builder::new_multi_thread()
@@ -5,9 +24,8 @@ fn main() {
         .enable_all()
         .build()
         .unwrap();
-    /*
-        rt.block_on(async {
-            run_server().await;
-        });
-    */
+
+    rt.block_on(async {
+        run_server().await;
+    });
 }
